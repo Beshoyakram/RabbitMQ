4cf8bd6a53a162a8b76d5f08541579815fcdaf16
Beshoy Akram <bakram@turndigital.net>
2025-01-18 17:38:21 +0200

    Init Project

---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver1 Name";
+
+IConnection conn = await factory.CreateConnectionAsync();
+IChannel channel = await conn.CreateChannelAsync();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
+await channel.QueueDeclareAsync(queueName, false, false, false, null);
+await channel.QueueBindAsync(queueName, exchangeName, routingKey);
+
+
+await channel.BasicQosAsync(0, 1, false);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (s, e) =>
+{
+    await Task.Delay(TimeSpan.FromSeconds(5));
+
+    var body = e.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received : {message}");
+    await channel.BasicAckAsync(e.DeliveryTag,false);
+};
+
+string consumerTag = await channel.BasicConsumeAsync(queueName, false, consumer);
+Console.ReadLine();
+
+await channel.BasicCancelAsync(consumerTag);
+
+await channel.CloseAsync();
+await conn.CloseAsync();
+await channel.DisposeAsync();
+await conn.DisposeAsync();
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender Name";
+
+IConnection conn =  await factory.CreateConnectionAsync();
+IChannel channel = await conn.CreateChannelAsync();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
+await channel.QueueDeclareAsync(queueName,false, false, false,null);
+await channel.QueueBindAsync(queueName,exchangeName,routingKey);
+
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine($"Sending Message #{i}");
+    byte[] Message = Encoding.UTF8.GetBytes($"Sending Message #{i}");
+    await channel.BasicPublishAsync(exchangeName, routingKey, Message);
+    Thread.Sleep(1000);
+
+}
+await channel.CloseAsync();
+await conn.CloseAsync();
+await channel.DisposeAsync();
+await conn.DisposeAsync();
